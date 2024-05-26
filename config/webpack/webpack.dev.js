80032e0295f8c605e6eef7eb0a9b6ae2b717684f
Jacob Thomas Redmond <127049997+Jrbiltmore@users.noreply.github.com>
2024-05-26 02:54:28 -0700

    Update webpack.dev.js

---
+++ config/webpack/webpack.dev.js
@@ -0,0 +1,25 @@
+// webpack.dev.js
+
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    historyApiFallback: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+});
