fea919c9349a8b50dc9622bd86f8ba65d93a9af6
Jacob Thomas Redmond <127049997+Jrbiltmore@users.noreply.github.com>
2024-05-26 03:04:48 -0700

    Update typeDefs.js
    Type Definitions:
    User: Defines the User type with id, name, and email fields.
    Query Definitions:
    users: Returns a list of all users.
    user: Returns a single user by ID.
    Mutation Definitions:
    addUser: Adds a new user.
    updateUser: Updates an existing user by ID.
    deleteUser: Deletes a user by ID.

---
+++ src/api/graphql/typeDefs.js
@@ -0,0 +1,25 @@
+// typeDefs.js
+
+const { gql } = require('apollo-server-express');
+
+// Type definitions
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
+  type Query {
+    users: [User]
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!): User
+    updateUser(id: ID!, name: String!, email: String!): User
+    deleteUser(id: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
